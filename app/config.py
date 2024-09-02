1e8deaac774b2eb9d1f056c4263d76866a9e7f72
emanuel-gauna <lemagauna@gmail.com>
2024-09-02 19:48:43 -0300

    agregando deploy de base de datos en supabase

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
     DB_NAME = os.getenv('DB_NAME')
     DB_USER = os.getenv('DB_USER')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
